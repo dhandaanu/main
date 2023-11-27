ef8eb1b4737cad72e61bd5f706c6aaf26b9c24d0
kirti <kirti@KMK>
2023-11-27 14:08:47 -0600

    Anu(2) + manali code merged

--- src/test/java/stepdefinition/Bclass.java
+++ src/test/java/stepdefinition/Bclass.java
@@ -1,11 +1,14 @@
 package stepdefinition;
 
+import pageobjects.ArrayPage;
 import pageobjects.Datastructure;
+import pageobjects.GraphPage;
 import pageobjects.HomePage;
 import pageobjects.HomePageCards;
 import pageobjects.Linkedlistcode;
 import pageobjects.Login;
 import pageobjects.Queue;
+import pageobjects.RegisterPage;
 import pageobjects.Stack;
 import pageobjects.Tree;
 
@@ -26,5 +29,11 @@ public class Bclass {
 	public Stack stack;
 	
 	public Tree tree;
+	
+	public ArrayPage fromarrayjava;
+	
+	public GraphPage fromgraphjava;
+	
+	public RegisterPage fromregisterjava;
 
 }
